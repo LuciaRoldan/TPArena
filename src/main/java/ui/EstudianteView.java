d0f704d2666433baa8098e811d36b15a92632eed
Unknown <lucia_roldan@outlook.com>
2018-06-29 18:44:38 -0300

    Constructores y main de MV y V

--- src/main/java/ui/EstudianteView.java
+++ src/main/java/ui/EstudianteView.java
@@ -1,5 +1,22 @@
 package ui;
 
-public class EstudianteView {
+import org.uqbar.arena.widgets.Panel;
+import org.uqbar.arena.windows.MainWindow;
 
+import domain.*;
+
+public class EstudianteView extends MainWindow<EstudianteViewModel>{
+
+	public EstudianteView() {
+	    super(new EstudianteViewModel(new Estudiante()));
+	  }
+
+	  @Override
+	  public void createContents(Panel mainPanel) {
+	  }
+
+	  public static void main(String[] args) {
+	    new EstudianteView().startApplication();
+	  }
+	  
 }
--- src/main/java/ui/EstudianteViewModel.java
+++ src/main/java/ui/EstudianteViewModel.java
@@ -1,5 +1,15 @@
 package ui;
 
+import org.uqbar.commons.model.annotations.Observable;
+
+import domain.*;
+
 public class EstudianteViewModel {
+
+	  private Estudiante estudiante;
+
+	  public EstudianteViewModel(Estudiante unEstudiante) {
+	    this.estudiante = unEstudiante;
+	  }
 
 }
