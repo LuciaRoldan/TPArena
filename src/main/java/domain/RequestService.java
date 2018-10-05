e0056182eba9208d385e3a5ce8c72a2ea42def7f
rociochipian <rociochipian@gmail.com>
2018-10-05 17:00:39 -0300

    get y post sin notas

--- src/main/java/domain/RequestService.java
+++ src/main/java/domain/RequestService.java
@@ -4,26 +4,34 @@ import javax.ws.rs.core.MediaType;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 
 public class RequestService {
 
 	private Client cliente;
     private static final String NOTITAS = "http://notitas.herokuapp.com/student";
+    private String TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIxMTEyMjIzMzMiLCJybmQiOiJ5SXNmZFIwN2lIR3BRRmVjYU9KT2VRPT0ifQ.9pVJGUXhrJPQ-TptNCt971l0h_1dWqWgMrHAWXJchho";
     
     
     public RequestService() {
         this.cliente = Client.create();
     }
     
-    public void cargarAlumno() {
-    	
+    public void cargarAlumno(int id, String input) {
+    	Client cliente = Client.create();
+    	WebResource web = cliente.resource(NOTITAS);
+    	ClientResponse response = web.header("Authorization", "Bearer " + TOKEN)
+    								 .accept(MediaType.APPLICATION_JSON)
+    								 .put(ClientResponse.class, input);
+    	System.out.println(response.getStatus());
     }
     
-    public Estudiante consultarAlumno(int legajo) {
-    	Estudiante estudiante = this.cliente.resource(NOTITAS)
-    			.path(String.valueOf(legajo))
-    			.accept(MediaType.APPLICATION_JSON)
-    			.get(Estudiante.class);
+    public ClientResponse consultarAlumno(int legajo) {
+    	WebResource web = cliente.resource(NOTITAS);
+    	ClientResponse estudiante = web.header("Authorization", "Bearer " + TOKEN)
+    								.accept(MediaType.APPLICATION_JSON)
+    								.get(ClientResponse.class);
+    	System.out.println(estudiante.getStatus());
     	return estudiante;
     }
     
--- src/test/java/requestService/TestsRS.java
+++ src/test/java/requestService/TestsRS.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.sun.jersey.api.client.ClientResponse;
+
 import domain.Estudiante;
 import domain.RequestService;
 
@@ -11,14 +13,24 @@ public class TestsRS {
 	
 	@Test
 	public void postDeUnAlumno() {
-		
+		RequestService rs = new RequestService();
+		rs.cargarAlumno(1214731, "{\r\n" + 
+    											"  \"code\": \"1214731\",\r\n" + 
+    											"  \"first_name\": \"Franco\",\r\n" + 
+    											"  \"last_name\": \"Bulgarelli\",\r\n" + 
+    											"  \"github_user\": \"flbulgarelli\"\r\n" + 
+    											"}");
+		assertTrue(true);
 	}
 
 	@Test
 	public void getDeUnAlumno() {
+		
 		RequestService rs = new RequestService();
-		Estudiante estudiante = rs.consultarAlumno(1214731);
-		assertEquals("Franco", estudiante.getNombre());
+		ClientResponse estudiante = rs.consultarAlumno(111222333);
+		System.out.println(estudiante.toString());
+		System.out.println(estudiante.getEntity(String.class));
+		assertTrue(true);
 	}
 
 }
